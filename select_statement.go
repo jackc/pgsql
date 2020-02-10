e07918cf3a1b5f3e0f0c5d24a9887b0dbba037ad
Jack Christensen <jack@jackchristensen.com>
2020-02-10 14:42:58 -0600

    Add new SelectStatement and remove old Statement

---
+++ select_statement.go
@@ -0,0 +1,171 @@
+// Package pgsql helps build SQL queries.
+package pgsql
+
+import (
+	"strconv"
+	"strings"
+)
+
+type SelectStatement struct {
+	selectClause  fooselectClause
+	fromClause    foofromClause
+	where         SQLWriter
+	orderByClause *fooorderClause
+	limit         int64
+	offset        int64
+}
+
+func Selectf(s string, args ...interface{}) *SelectStatement {
+	return (&SelectStatement{}).Selectf(s, args...)
+}
+
+func Fromf(s string, args ...interface{}) *SelectStatement {
+	return (&SelectStatement{}).Fromf(s, args...)
+}
+
+func (ss *SelectStatement) Selectf(s string, args ...interface{}) *SelectStatement {
+	ss.selectClause = fooselectClause{
+		exprList: []SQLWriter{&FormatString{s: s, args: args}},
+	}
+
+	return ss
+}
+
+func (ss *SelectStatement) Distinct(b bool) *SelectStatement {
+	ss.selectClause.isDistinct = b
+	if !b {
+		ss.selectClause.distinctOnExprList = nil
+	}
+
+	return ss
+}
+
+func (ss *SelectStatement) DistinctOnf(s string, args ...interface{}) *SelectStatement {
+	ss.selectClause.isDistinct = true
+	ss.selectClause.distinctOnExprList = []SQLWriter{&FormatString{s: s, args: args}}
+
+	return ss
+}
+
+func (ss *SelectStatement) Fromf(s string, args ...interface{}) *SelectStatement {
+	ss.fromClause = foofromClause{
+		from: &FormatString{s: s, args: args},
+	}
+
+	return ss
+}
+
+func (ss *SelectStatement) Where(cond SQLWriter) *SelectStatement {
+	if ss.where == nil {
+		ss.where = cond
+	} else {
+		ss.where = &BinaryExpr{Left: ss.where, Op: "and", Right: cond}
+	}
+	return ss
+}
+
+func (ss *SelectStatement) Wheref(s string, args ...interface{}) *SelectStatement {
+	return ss.Where(&FormatString{s: s, args: args})
+}
+
+func (ss *SelectStatement) Order(order SQLWriter) *SelectStatement {
+	ss.orderByClause = &fooorderClause{exprList: []SQLWriter{order}}
+	return ss
+}
+
+func (ss *SelectStatement) Orderf(s string, args ...interface{}) *SelectStatement {
+	return ss.Order(&FormatString{s: s, args: args})
+}
+
+func (ss *SelectStatement) Limit(n int64) *SelectStatement {
+	ss.limit = n
+	return ss
+}
+
+func (ss *SelectStatement) Offset(n int64) *SelectStatement {
+	ss.offset = n
+	return ss
+}
+
+func (ss *SelectStatement) WriteSQL(sb *strings.Builder, args *Args) {
+	ss.selectClause.WriteSQL(sb, args)
+	ss.fromClause.WriteSQL(sb, args)
+	if ss.where != nil {
+		sb.WriteString(" where ")
+		ss.where.WriteSQL(sb, args)
+	}
+	if ss.orderByClause != nil {
+		ss.orderByClause.WriteSQL(sb, args)
+	}
+	if ss.limit != 0 {
+		sb.WriteString(" limit ")
+		sb.WriteString(strconv.FormatInt(ss.limit, 10))
+	}
+	if ss.offset != 0 {
+		sb.WriteString(" offset ")
+		sb.WriteString(strconv.FormatInt(ss.offset, 10))
+	}
+}
+
+type fooselectClause struct {
+	isDistinct         bool
+	distinctOnExprList []SQLWriter
+	exprList           []SQLWriter
+}
+
+func (s fooselectClause) WriteSQL(sb *strings.Builder, args *Args) {
+	sb.WriteString("select")
+	if s.isDistinct {
+		sb.WriteString(" distinct")
+	}
+
+	if len(s.distinctOnExprList) > 0 {
+		sb.WriteString(" on (")
+		for i, e := range s.distinctOnExprList {
+			if i > 0 {
+				sb.WriteString(", ")
+			}
+			e.WriteSQL(sb, args)
+		}
+		sb.WriteString(")")
+	}
+
+	sb.WriteString(" ")
+	if len(s.exprList) > 0 {
+		for i, e := range s.exprList {
+			if i > 0 {
+				sb.WriteString(", ")
+			}
+			e.WriteSQL(sb, args)
+		}
+	} else {
+		sb.WriteString("*")
+	}
+}
+
+type foofromClause struct {
+	from SQLWriter
+}
+
+func (f foofromClause) WriteSQL(sb *strings.Builder, args *Args) {
+	if f.from == nil {
+		return
+	}
+
+	sb.WriteString(" from ")
+	f.from.WriteSQL(sb, args)
+}
+
+type fooorderClause struct {
+	exprList []SQLWriter
+}
+
+func (o fooorderClause) WriteSQL(sb *strings.Builder, args *Args) {
+	sb.WriteString(" order by ")
+	for i, e := range o.exprList {
+		if i > 0 {
+			sb.WriteString(", ")
+		}
+		e.WriteSQL(sb, args)
+	}
+}
---
+++ select_statement_test.go
@@ -0,0 +1,63 @@
+package pgsql_test
+
+import (
+	"testing"
+
+	"github.com/jackc/pgsql"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectf(t *testing.T) {
+	a := pgsql.Selectf("a, b, c")
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select a, b, c", sql)
+	assert.Empty(t, args)
+}
+
+func TestFromf(t *testing.T) {
+	a := pgsql.Fromf("people")
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select * from people", sql)
+	assert.Empty(t, args)
+}
+
+func TestSelectStatementDistinct(t *testing.T) {
+	a := pgsql.Selectf("a, b, c").Distinct(true)
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select distinct a, b, c", sql)
+	assert.Empty(t, args)
+}
+
+func TestSelectStatementDistinctOn(t *testing.T) {
+	a := pgsql.Selectf("a, b, c").DistinctOnf("a, b")
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select distinct on (a, b) a, b, c", sql)
+	assert.Empty(t, args)
+}
+
+func TestSelectStatementWhere(t *testing.T) {
+	a := pgsql.Selectf("a, b, c").Fromf("t").Wheref("foo=?", 42)
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select a, b, c from t where foo=$1", sql)
+	assert.Equal(t, []interface{}{42}, args)
+}
+
+func TestSelectStatementOrder(t *testing.T) {
+	a := pgsql.Selectf("a, b, c").Fromf("t").Orderf("c desc")
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select a, b, c from t order by c desc", sql)
+	assert.Empty(t, args)
+}
+
+func TestSelectStatementLimitAndOffset(t *testing.T) {
+	a := pgsql.Selectf("a, b, c").Fromf("t").Orderf("c desc")
+	a.Limit(5)
+	sql, args := pgsql.Build(a)
+	assert.Equal(t, "select a, b, c from t order by c desc limit 5", sql)
+	assert.Empty(t, args)
+
+	a.Offset(10)
+	sql, args = pgsql.Build(a)
+	assert.Equal(t, "select a, b, c from t order by c desc limit 5 offset 10", sql)
+	assert.Empty(t, args)
+}
